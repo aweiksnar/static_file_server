e64fb3f55fbd75cb6fad48d31a20925329d091d2
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-11-14 21:16:58 -0600

    normalize path and add validity check

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
 var http = require("http");
 var parse = require("url").parse;
-var join = require("path").join;
+var path = require("path");
 var fs = require("fs");
+var join = path.join;
+var normalize = path.normalize;
 
 var root = __dirname
 
 var server = http.createServer(function(req, res){
   var url = parse(req.url);
-  var path = join(root, url.pathname);
-  
-  fs.stat(path, function(err, stats){
-    if (err) {
-      if ("ENOENT" == err.code) {
-        res.statusCode = 404;
-        res.end("Page not found")
+  var reqPath = normalize(join(root, url.pathname));
+
+  if (pathIsValid(reqPath)) {
+    fs.stat(reqPath, function(err, stats){
+      if (err) {
+        if ("ENOENT" == err.code) {
+          res.statusCode = 404;
+          res.end("Page not found")
+        } else {
+          res.statusCode = 500;
+          res.end("Internal Server Error");
+        }
       } else {
-        res.statusCode = 500;
-        res.end("Internal Server Error");
+        var stream = fs.createReadStream(reqPath);
+        stream.pipe(res);
+        stream.on("error", function(err){
+          res.statusCode = 500;
+          res.end("Internal Server Error");
+        });
       }
-    } else {
-      var stream = fs.createReadStream(path);
-      stream.pipe(res);
-      stream.on("error", function(err){
-        res.statusCode = 500;
-        res.end("Internal Server Error");
-      });
-    }
-  })
-  
+    });
+  } else {
+    console.log("ATTACKER")
+  }
 });
+
+function pathIsValid(reqPath) {
+  return (reqPath.indexOf("..") && path.dirname(reqPath) == __dirname);
+}
 
 var port = 4567
 server.listen(port);
